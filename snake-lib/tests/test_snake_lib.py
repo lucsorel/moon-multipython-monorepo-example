f24b6658322145f9e4111c97a80d7d190da41cdf
Luc Sorel-Giffo <luc.sorel@gmail.com>
2025-09-05 17:49:53 +0200

    ci: change test, add task inputs for caching

--- snake-lib/tests/test_snake_lib.py
+++ snake-lib/tests/test_snake_lib.py
@@ -1,4 +1,4 @@
 from snake_lib import hello
 
 def test_hello():
-    assert hello() == "Hello from snake-lib!", "should say hello"
+    assert hello() == "Hello from snake-lib! :)", "should say hello"
